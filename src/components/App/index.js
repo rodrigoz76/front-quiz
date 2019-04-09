a3492b874907f78f97be3de850f057fe1302244d
rodrigoz76 <rodasproducciones@gmail.com>
2019-04-09 09:51:22 -0300

    ejercicio resuelto R Zabala v2

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import Shelf from '../Shelf';
 import Filter from '../Shelf/Filter';
 import FloatCart from '../FloatCart';
+import Header from '../Header';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <main>
+          <Header />
           <Filter />
           <Shelf />
         </main>
